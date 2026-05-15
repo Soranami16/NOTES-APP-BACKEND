abc453feb6643b4f330f2f161e44e79ec3ad25e4
Salamah <fthira.sal@gmail.com>
2026-05-15 18:08:08 +0700

    tambah cors

--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,12 @@ import 'dotenv/config';
 import express from 'express';
 import routes from '../routes/index.js';
 import ErrorHandler from '../middlewares/error.js';
- 
+const cors = require('cors');
 const app = express();
  
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler);
+app.use(cors());
  
 export default app;
