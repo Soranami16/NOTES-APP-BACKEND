0a395455bc535e18c565a414a08597e8e390354f
Salamah <fthira.sal@gmail.com>
2026-05-15 18:49:51 +0700

    si httpss ituuu

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,13 @@ import ErrorHandler from '../middlewares/error.js';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*', // Izinkan semua sumber
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}));
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler);
