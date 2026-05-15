79f0fcd705cbc01d9862c2772253162310be4632
Salamah <fthira.sal@gmail.com>
2026-05-15 18:22:37 +0700

    fix: force update case sensitivity for noteRepositories

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,10 @@ import 'dotenv/config';
 import express from 'express';
 import routes from '../routes/index.js';
 import ErrorHandler from '../middlewares/error.js';
-const cors = require('cors');
+import cors from 'cors';
+
 const app = express();
- 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler);
